724dedcb7a726be745a6060c00d508ee5640164d
Shaurya Vaid <shauryavaid1@gmail.com>
2026-07-05 21:06:52 +0530

    Add/Update DSA practice files

---
+++ 2D Arrays/Traversing_in_2d.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+void traverse(){
+    int arr[3][4];
+    int n=3, m=4;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+        }
+    }
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+int main(){
+    traverse();
+}
--- Arrays/Buy_Sell_Stock.cpp
+++ Arrays/Buy_Sell_Stock.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<climits>
 using namespace std;
-void maxSubarr(int *prices,int n){
+/*void maxSubarr(int *prices,int n){
     int Bestbuy[100000];
     Bestbuy[0]=INT_MAX;
     for(int i=1; i<n; i++){
@@ -15,10 +15,52 @@ void maxSubarr(int *prices,int n){
     }
     cout<<"profit: "<<Maxprofit;
 }
+//tapwater question added too.
+void TRAPWATER(int *prices,int n){
+    //consider prices array as a height array.
+    int leftMax[20000];
+    leftMax[0]=prices[0];
+    for(int i=1; i<n; i++){
+        leftMax[i]= max(leftMax[i-1], prices[i]);
+    }
+
+    int rightMax[20000];
+    rightMax[n-1]=prices[n-1];
+    for(int i=n-2; i>=0; i--){
+        rightMax[i]= max(rightMax[i+1], prices[i]);
+    }
+
+    int WaterTraped=0;
+    for(int i=0; i<n; i++){
+        int currWater= (min(leftMax[i], rightMax[i]))-prices[i];
+        if(currWater > 0){
+            WaterTraped+= currWater;
+        }
+    }
+    cout<<"WaterTraped= "<<WaterTraped<<endl;
+
+}
+*/
+//Majority Element question added too.
+void MajElement(int *prices,int n){
+    //consider prices array as a nums array.
+
+    for(int i=0; i<n; i++){
+        int count=0;
+        for(int j=0; j<n; j++){
+            if(prices[i]==prices[j]){
+                count++;
+            }
+        }
+        if(count>(n/2)){
+            cout<<prices[i]<<endl;
+        }
+    }
+}
 
 int main(){
-    int prices[6]={7, 1, 5, 3, 6, 4};
+    int prices[3]={3, 2, 3};
     int n= sizeof(prices)/sizeof(int);
-    maxSubarr(prices, n);
+    MajElement(prices, n);
     return 0;
 }
